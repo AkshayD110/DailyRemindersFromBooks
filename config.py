3e013071958a3d2c822f1baeed46a883a1595de6
Akshay <akshayd110@gmail.com>
2022-02-02 20:33:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-sender_email = ${{secrets.SENDER_MAIN}}
-receiver_email = ${{secrets.RECEIVER_MAIL}}
-password = ${{secrets.LOGIN}}
+sender_email = ${{ secrets.SENDER_MAIN }}
+receiver_email = ${{ secrets.RECEIVER_MAIL }}
+password = ${{ secrets.LOGIN }}
 number_of_highlights_per_day = 10
