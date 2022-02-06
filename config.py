76a1ccd3d68b50727a847f512fbe79dbc61790da
Akshay <akshayd110@gmail.com>
2022-02-06 09:58:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-sender_email = os.environ("sender_email")
-receiver_email = os.environ("receiver_email")
-password = os.environ("password")
+sender_email = os.environ.get("sender_email")
+receiver_email = os.environ.get("receiver_email")
+password = os.environ.get("password")
 number_of_highlights_per_day = 10
