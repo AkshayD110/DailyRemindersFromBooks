cb4615809831501aeeb86970d660123b03fbda7f
Akshay <akshayd110@gmail.com>
2022-02-06 16:04:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 sender_email = "perfengineeringstuff"
-receiver_email = os.environ['RECEIVER_MAIL']
+receiver_email = "akshayd110@gmail.com"
 password = os.environ['LOGIN']
 number_of_highlights_per_day = 10
